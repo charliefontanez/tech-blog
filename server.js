2ea2aa15fe51315a0866641789042de38ea62c5a
Carlos Fontanez <charliefontanezis@gmail.com>
2022-07-06 22:00:18 -0400

    setting up handlebars

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ require('dotenv').config();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(routes);
