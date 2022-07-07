e21b0b49ce16e87bfd962c2f4b1fc304f03659bc
Carlos Fontanez <charliefontanezis@gmail.com>
2022-07-06 21:41:58 -0400

    server initalized

--- server.js
+++ server.js
@@ -13,7 +13,6 @@ require('dotenv').config();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(routes);
