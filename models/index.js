0e4bc47241f41e1f7e3ce433dc1416c5d0be07ae
Carlos Fontanez <charliefontanezis@gmail.com>
2022-07-07 19:28:46 -0400

    working on partials

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const Post = require('./Post');
 const User = require('./User');
+const Comment = require('./Comment');
 
 Post.belongsTo(User, {
   foreignKey: 'user_id'
@@ -9,4 +10,20 @@ User.hasMany(Post, {
   foreignKey: 'user_id'
 });
 
-module.exports = { Post, User };
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+})
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id'
+})
+
+module.exports = { Post, User, Comment };
