c8193741cb7ddcb00a276e20861fcade95956095
Carlos Fontanez <charliefontanezis@gmail.com>
2022-07-11 13:47:15 -0400

    add post feature

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -8,24 +8,25 @@ function newPostClick() {
 
 async function createPost(event) {
   event.preventDefault();
-  const title = document.querySelector('name=["title"]').value.trim();
-  const contents = document.querySelector('name=["post-content"]').value.trim();
+  const title = document.querySelector('input[name="title"]').value.trim();
+  const content = document.querySelector('#content-input').value.trim();
 
-  if (title && contents) {
+  if (title && content) {
     const response = await fetch('/api/posts', {
       method: 'POST',
       body: JSON.stringify({
         title,
-        contents
+        content
       }),
       headers: { 'Content-Type': 'application/json' }
     });
 
     if (response.ok) {
-      console.log('post created');
+      console.log('post created'0;)
+      document.location.replace('/dashboard');
     }
     else {
-      console.log('error');
+      alert
     }
   }
 }
